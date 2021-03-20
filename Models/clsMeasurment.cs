168a9e05ad6226090d4143e407eda105c5f5fa6d
Abdul Mateen Khan <abdulmateen50@gmail.com>
2021-03-20 16:06:58 +0530

    Add method for garment measurment

---
+++ Models/clsMeasurment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smart_Tailoring_WebAPI.Models
+{
+    public class clsMeasurment
+    {
+        public int MeasurmentID { get; set; }
+        public string MeasurmentName { get; set; }
+        public bool IsMendatory { get; set; }
+    }
+}
